c722d7a87bb08421486bea3f93c2587cc72c5d6a
reduan <redu1996@gmail.com>
2020-12-07 20:56:28 +0100

    Añade constructor por defecto y parametrizado, cambio de nombre de los atributos y encapsulamiento de los mismos.

--- src/main/java/tarea4/Ejemplos.java
+++ src/main/java/tarea4/Ejemplos.java
@@ -11,8 +11,18 @@ package tarea4;
  */
 public class Ejemplos {
 
-    int ejemplo1;
-    int ejemplo2;
-    int ejemplo3;
+    private int atributo1;
+    private int atributo2;
+    private int atributo3;
 
+    public Ejemplos() {
+        
+    }
+
+    public Ejemplos(int atributo1, int atributo2, int atributo3) {
+        this.atributo1 = atributo1;
+        this.atributo2 = atributo2;
+        this.atributo3 = atributo3;
+    }
+    
 }
