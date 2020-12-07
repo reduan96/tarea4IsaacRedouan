1baec9d416aa724b11bdf7909a7fb3d7cf65e7dd
Isaac <jimenezmerino.isaac9871@gmail.com>
2020-12-07 20:58:29 +0100

    Añade Getters y Setters a la clase Ejemplos

--- src/main/java/tarea4/Ejemplos.java
+++ src/main/java/tarea4/Ejemplos.java
@@ -24,5 +24,32 @@ public class Ejemplos {
         this.atributo2 = atributo2;
         this.atributo3 = atributo3;
     }
+
+    public int getAtributo1() {
+        return atributo1;
+    }
+
+    public void setAtributo1(int atributo1) {
+        this.atributo1 = atributo1;
+    }
+
+    public int getAtributo2() {
+        return atributo2;
+    }
+
+    public void setAtributo2(int atributo2) {
+        this.atributo2 = atributo2;
+    }
+
+    public int getAtributo3() {
+        return atributo3;
+    }
+
+    public void setAtributo3(int atributo3) {
+        this.atributo3 = atributo3;
+    }
+    
+    
+    
     
 }
